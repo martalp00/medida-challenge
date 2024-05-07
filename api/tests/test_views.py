4db51cc182ae0ba0630fcb3eae14a7eb49d1235a
martalp00 <martalo1304@gmail.com>
2024-05-08 01:53:02 +0200

    Filter in views, pytest and diagram

---
+++ api/tests/test_views.py
@@ -0,0 +1,171 @@
+import pdb
+from django.test import Client
+from django.urls import reverse
+import datetime
+from unittest.mock import MagicMock
+from unittest.mock import patch
+import pytest
+import requests
+from api.views import eventEndpoint
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
+# Create your tests here.
+
+@pytest.mark.django_db
+def test_eventEndpoint_with_empty_response():
+    mock_response = MagicMock()
+    mock_response.json.return_value = []
+    requests_get_mock = MagicMock(return_value=mock_response)
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("requests.get", requests_get_mock)
+
+        request_factory = APIRequestFactory()
+        request = request_factory.post('http://localhost:8000/events/', {'league': 'NFL', 'startDate': '2023-01-01', 'endDate': '2023-01-31'})
+
+        response = eventEndpoint(request)
+
+    assert response.status_code == 200
+    assert response.data == []
+
+
+@pytest.mark.django_db
+def test_eventEndpoint_with_ConnectionError():
+    requests_get_mock = MagicMock(side_effect=requests.ConnectionError("Failed to connect to API"))
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("requests.get", requests_get_mock)
+
+        request_factory = APIRequestFactory()
+        request = request_factory.post('http://localhost:8000/events/', {'league': 'NFL', 'startDate': '2023-01-01', 'endDate': '2023-01-31'})
+
+        response = eventEndpoint(request)
+
+    assert response.status_code == 503
+
+
+@pytest.mark.django_db
+def test_eventEndpoint_with_Exception():
+    requests_get_mock = MagicMock(side_effect=Exception("Internal Error"))
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("requests.get", requests_get_mock)
+
+        request_factory = APIRequestFactory()
+        request = request_factory.post('http://localhost:8000/events/', {'league': 'NFL', 'startDate': '2023-01-01', 'endDate': '2023-01-31'})
+
+        response = eventEndpoint(request)
+
+    assert response.status_code == 500
+
+
+@pytest.mark.django_db
+def test_eventEndpoint_with_data_correct_param():
+    mock_scoreboard_response = MagicMock()
+    mock_scoreboard_response.json.return_value = [
+       {
+            'away': {'city': 'B', 'id': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35', 'nickName': 'Team B'},
+            'home': { 'city': 'A', 'id': '8da0c96d-7b3d-41f3-9e68-29607f3babcf', 'nickName': 'Team A'},
+            'id': '5055c2a2-af68-4082-9834-ceb36dd0a807',
+            'timestamp': '2023-01-11T14:00:00Z'
+        },
+        {
+            'away': { 'city': 'A', 'id': '8da0c96d-7b3d-41f3-9e68-29607f3babcf', 'nickName': 'Team A'},
+            'home': {'city': 'B', 'id': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35', 'nickName': 'Team B'},
+            'id': '5055c2a2-af68-4082-9834-ceb36dd0a807',
+            'timestamp': '2023-02-11T18:00:00Z'
+        },
+    ]
+
+    mock_team_rankings_response = MagicMock()
+    mock_team_rankings_response.json.return_value =  [
+        {'rank': 1, 'rankPoints': 100.3, 'teamId': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35'},
+        {'rank': 2, 'rankPoints': 90, 'teamId': '8da0c96d-7b3d-41f3-9e68-29607f3babcf'},
+    ]
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("requests.get", MagicMock(side_effect=lambda url: mock_scoreboard_response
+                                             if url == 'http://localhost:9000/NFL/scoreboard' else mock_team_rankings_response))       
+
+        request_factory = APIRequestFactory()
+        request = request_factory.post('http://localhost:8000/events/', {'league': 'NFL', 'startDate': '2023-01-01', 'endDate': '2023-01-31'})
+
+        response = eventEndpoint(request)
+    assert response.status_code == 200
+    assert response.data == [
+    {
+        "eventId": "5055c2a2-af68-4082-9834-ceb36dd0a807",
+        "eventDate": "2023-01-11",
+        "eventTime": "14:00:00",
+        "homeTeamId": "8da0c96d-7b3d-41f3-9e68-29607f3babcf",
+        "homeTeamNickName": "Team A",
+        "homeTeamCity": "A",
+        "homeTeamRank": 2,
+        "homeTeamRankPoints": 90,
+        "awayTeamId": "ae5132a4-e4b2-4bda-9933-b75c542b8d35",
+        "awayTeamNickName": "Team B",
+        "awayTeamCity": "B",
+        "awayTeamRank": 1,
+        "awayTeamRankPoints": 100.3
+    }]
+
+
+@pytest.mark.django_db
+def test_eventEndpoint_with_data_incorrect_date_param():
+    mock_scoreboard_response = MagicMock()
+    mock_scoreboard_response.json.return_value = [
+       {
+            'away': {'city': 'B', 'id': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35', 'nickName': 'Team B'},
+            'home': { 'city': 'A', 'id': '8da0c96d-7b3d-41f3-9e68-29607f3babcf', 'nickName': 'Team A'},
+            'id': '5055c2a2-af68-4082-9834-ceb36dd0a807',
+            'timestamp': '2023-01-11T14:00:00Z'
+        },
+    ]
+
+    mock_team_rankings_response = MagicMock()
+    mock_team_rankings_response.json.return_value =  [
+        {'rank': 1, 'rankPoints': 100.3, 'teamId': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35'},
+        {'rank': 2, 'rankPoints': 90, 'teamId': '8da0c96d-7b3d-41f3-9e68-29607f3babcf'},
+    ]
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("requests.get", MagicMock(side_effect=lambda url: mock_scoreboard_response
+                                             if url == 'http://localhost:9000/NFL/scoreboard' else mock_team_rankings_response))       
+
+        request_factory = APIRequestFactory()
+        request = request_factory.post('http://localhost:8000/events/', {'league': 'NFL', 'startDate': '2023-03-15', 'endDate': '2023-01-31'})
+
+        response = eventEndpoint(request)
+    assert response.status_code == 200
+    assert response.data == []
+
+
+@pytest.mark.django_db
+def test_eventEndpoint_with_data_incorrect_league_param():
+    mock_scoreboard_response = MagicMock()
+    mock_scoreboard_response.json.return_value = [
+       {
+            'away': {'city': 'B', 'id': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35', 'nickName': 'Team B'},
+            'home': { 'city': 'A', 'id': '8da0c96d-7b3d-41f3-9e68-29607f3babcf', 'nickName': 'Team A'},
+            'id': '5055c2a2-af68-4082-9834-ceb36dd0a807',
+            'timestamp': '2023-01-11T14:00:00Z'
+        },
+    ]
+
+    mock_team_rankings_response = MagicMock()
+    mock_team_rankings_response.json.return_value =  [
+        {'rank': 1, 'rankPoints': 100.3, 'teamId': 'ae5132a4-e4b2-4bda-9933-b75c542b8d35'},
+        {'rank': 2, 'rankPoints': 90, 'teamId': '8da0c96d-7b3d-41f3-9e68-29607f3babcf'},
+    ]
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("requests.get", MagicMock(side_effect=lambda url: mock_scoreboard_response
+                                             if url == 'http://localhost:9000/NFL/scoreboard' else mock_team_rankings_response))       
+
+        request_factory = APIRequestFactory()
+        request = request_factory.post('http://localhost:8000/events/', {'league': 'UEFA', 'startDate': '2023-01-01', 'endDate': '2023-01-31'})
+
+        response = eventEndpoint(request)
+    assert response.status_code == 200
+    assert response.data == []
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
+import pdb
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Event, EventsRequest, EventsResponse
 from .serializer import EventsResponseSerializer
 import requests
@@ -9,45 +11,75 @@ import datetime
 
 @api_view(['POST'])
 def eventEndpoint(request):
-    # First endpoint call
-    scoreboard_response = requests.get('http://localhost:9000/NFL/scoreboard')
-    scoreboard_data = scoreboard_response.json()
+    try:
+        # Get parameters
+        params = request.data
+        league = params.get('league')
+        start_date_str = params.get('startDate')
+        end_date_str = params.get('endDate')
 
-    # Second endpoint call
-    team_rankings_response = requests.get('http://localhost:9000/NFL/team-rankings')
-    team_rankings_data = team_rankings_response.json()
+        start_date = datetime.datetime.strptime(start_date_str, "%Y-%m-%d").date() if start_date_str else None
+        end_date = datetime.datetime.strptime(end_date_str, "%Y-%m-%d").date() if end_date_str else None
 
-    # filter for the selected fields
-    response_data = []
+        # First endpoint call
+        scoreboard_response = requests.get('http://localhost:9000/NFL/scoreboard')
+        scoreboard_data = scoreboard_response.json()
 
-    for event in scoreboard_data:
-        timestamp_str = event['timestamp']
-        timestamp = datetime.datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
-        
-        #home_teamRank, home_teamRankPoints = eventSearchRankPoints(event['home']['id'], team_rankings_data)
-        home_teamRank, home_teamRankPoints, away_teamRank, away_teamRankPoints = eventSearchRankPoints(event['home']['id'],event['away']['id'], team_rankings_data)
+        # Second endpoint call
+        team_rankings_response = requests.get('http://localhost:9000/NFL/team-rankings')
+        team_rankings_data = team_rankings_response.json()
+        #pdb.set_trace()
+        # filter for the selected fields
+        response_data = []
 
-        event_data = {
-            'eventId' : event['id'],
-            'eventDate' : timestamp.date(),
-            'eventTime' : timestamp.time(),
-            'homeTeamId' : event['home']['id'],
-            'homeTeamNickName' : event['home']['nickName'],
-            'homeTeamCity' : event['home']['city'],
-            'homeTeamRank' : home_teamRank,
-            'homeTeamRankPoints' : home_teamRankPoints,
-            'awayTeamId' : event['away']['id'],
-            'awayTeamNickName' : event['away']['nickName'],
-            'awayTeamCity' : event['away']['city'],
-            'awayTeamRank' : away_teamRank,
-            'awayTeamRankPoints' :  away_teamRankPoints,
-        }
+        for event in scoreboard_data:
+            timestamp_str = event['timestamp']
+            timestamp = datetime.datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
+            
+            if event_matches_params(league, start_date, end_date, timestamp.date()):
+                home_teamRank, home_teamRankPoints, away_teamRank, away_teamRankPoints = eventSearchRankPoints(event['home']['id'],event['away']['id'], team_rankings_data)
 
-        response_data.append(event_data)
+                event_data = {
+                    'eventId' : event['id'],
+                    'eventDate' : timestamp.date(),
+                    'eventTime' : timestamp.time(),
+                    'homeTeamId' : event['home']['id'],
+                    'homeTeamNickName' : event['home']['nickName'],
+                    'homeTeamCity' : event['home']['city'],
+                    'homeTeamRank' : home_teamRank,
+                    'homeTeamRankPoints' : home_teamRankPoints,
+                    'awayTeamId' : event['away']['id'],
+                    'awayTeamNickName' : event['away']['nickName'],
+                    'awayTeamCity' : event['away']['city'],
+                    'awayTeamRank' : away_teamRank,
+                    'awayTeamRankPoints' :  away_teamRankPoints,
+                }
 
-    serializer = EventsResponseSerializer(data=response_data, many=True)
-    serializer.is_valid(raise_exception=True)
-    return Response(serializer.data)
+                response_data.append(event_data)
+
+        serializer = EventsResponseSerializer(data=response_data, many=True)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data)
+
+    except requests.ConnectionError as e:
+        error_message = f"Failed to connect to API: {str(e)}"
+        return Response({"error": error_message}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+    except Exception as e:
+        error_message = f"Internal Error: {str(e)}"
+        return Response({"error": error_message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def event_matches_params(league, start_date, end_date, event_date):
+    # As the Third Party API mock does not filter, it is filetered here
+    if league and 'NFL' != league:
+        return False
+    if start_date and event_date < start_date:
+        return False
+    if end_date and event_date > end_date:
+        return False
+    return True
+
 
 def eventSearchRankPoints(home_teamId, away_teamId, team_rankings_data: list[str]):
     otherDataFound = False
